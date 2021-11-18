bb1cac4579754c5654ee618e4ea461a5ec0fcbe1
dxndvndxn <danjablooo1@gmail.com>
2021-11-18 21:19:02 +0300

    invert && unique

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (!obj) {
+    return obj
+  }
+  else if (!Object.keys(obj).length) {
+    return obj
+  } else {
+    return Object.entries(obj).reduce((previous, [currentKey, currentValue]) => ({ ...previous, [currentValue]: currentKey }), {})
+  }
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,5 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  return Array.isArray(arr) ? [...new Set(arr)] : []
 }
