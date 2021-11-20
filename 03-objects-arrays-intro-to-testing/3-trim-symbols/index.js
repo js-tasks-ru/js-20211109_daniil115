f6dc4ed2982a0297a9478ce04a003c1efb1f1db9
dxndvndxn <danjablooo1@gmail.com>
2021-11-20 14:50:33 +0300

    Trim symbols and homework 4

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (string === '' || size <= 0) return ''
+  if (string.length && !size) return string
 
+  const strToArr = string.split('')
+  let localSize = 0
+  let totalStr = ''
+
+  strToArr.forEach((letter, i) => {
+    if (i === 0) {
+      totalStr += letter
+      if (strToArr[i + 1] === letter) {
+        localSize += 1
+      }
+    }
+    else if (i > 0) {
+      if (strToArr[i - 1] === letter && localSize < size) {
+        totalStr += letter
+        localSize += 1
+      }
+      else if (strToArr[i - 1] !== letter) {
+        totalStr += letter
+        localSize = 1
+      }
+    }
+  })
+
+  return totalStr
 }
