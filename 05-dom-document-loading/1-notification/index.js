4b9f7efb8d25ea3bc84996ec2cc7a5e9e08a5a56
dxndvndxn <danjablooo1@gmail.com>
2021-11-25 21:17:37 +0300

    homework 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,49 @@
 export default class NotificationMessage {
+  constructor (message, { duration = 0, type = 'error' } = {}) {
+    this.duration = duration
+    this.type = type
+    this.message = message
+    this.id = 'added'
+    this.show()
+  }
 
+  getTemplate () {
+    return `
+      <div id="${ this.id }" class="notification ${ this.type }" style="--value:${ this.humanTime }">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">
+              ${ this.type }
+          </div>
+          <div class="notification-body">
+              ${ this.message }
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  show (div) {
+    const element = !div ? document.createElement('div') : div
+    element.innerHTML = this.getTemplate()
+    this.element = element.firstElementChild
+    const isExist = document.querySelector(`#${ this.id }`)
+    !isExist ? document.body.append(this.element) : null
+
+    this.remove()
+  }
+
+  destroy () {
+    this.element.remove()
+  }
+
+  get humanTime () {
+    return `${this.duration / 1000}s`
+  }
+
+  remove () {
+    setTimeout(() => {
+      this.destroy()
+    }, this.duration)
+  }
 }
