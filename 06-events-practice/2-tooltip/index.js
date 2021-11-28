412abd093efd7e8615b23ad04ecb398b0ef99a22
dxndvndxn <danjablooo1@gmail.com>
2021-11-28 14:52:10 +0300

    homework 6

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,54 @@
 class Tooltip {
+  static checkInstance
 
+  constructor() {
+    if (!Tooltip.checkInstance) {
+      Tooltip.checkInstance = this
+    } else {
+      return Tooltip.checkInstance
+    }
+  }
+
+  get template () {
+    return `
+     <div class="tooltip">
+       ${ this.tooltipInner }
+     </div>
+    `
+  }
+
+  render (value = '') {
+    const element = document.createElement('div')
+    this.tooltipInner = value
+    element.innerHTML = this.template
+    this.element = element.firstElementChild
+    document.body.append(this.element)
+  }
+
+  initialize () {
+    document.addEventListener('pointerover', this.overEvent.bind(this))
+    document.addEventListener('pointerout', this.outEvent.bind(this))
+  }
+
+  overEvent (e) {
+    const isToolTip = e.target.dataset.tooltip
+
+    if (isToolTip) this.render(e.target.dataset.tooltip)
+  }
+
+  outEvent (e) {
+    const isToolTip = e.target.dataset.tooltip
+
+    if (isToolTip) {
+      this.destroy()
+      document.removeEventListener('pointerover', this.overEvent.bind(this))
+      document.removeEventListener('pointerout', this.outEvent.bind(this))
+    }
+  }
+
+  destroy () {
+    this.element.remove()
+  }
 }
 
 export default Tooltip;
